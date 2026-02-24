a5247767713e39ea9a8ea90c0e67f8796de994bb
iskanderab <iskanderabasi@gmail.com>
2026-02-24 04:20:54 +0100

    feat: create staging with args

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,9 @@ FROM node:22-alpine
 WORKDIR /app
 
 RUN npm install -g yarn@1 --force
-
 COPY package*.json .
 
-RUN yarn 
+ARG ENV
+RUN if [ "$ENV" = "production"]; then yarn install --prod; else yarn install; fi
 
 COPY . .
-
-CMD ["yarn", "dev"]
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+import express from "express"
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => { 
   console.log("Hello from debugger"); 
-  res.send('Hello World iskander test sidi aliii + asilla ');
+  res.send('Hello World iskander test sidi aliii + asilla +++  ');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is not running on port ${PORT}`);
+  console.log(`Server is not running on port : ${PORT}`);
 });
